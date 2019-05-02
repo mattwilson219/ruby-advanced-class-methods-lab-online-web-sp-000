584dfde4c8cce930b81143afe634e04da240bd00
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-02 18:03:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,4 +52,12 @@ class Song
     song
   end
 
+  def self.create_from_filename
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song.save
+    song
+  end
+
 end
